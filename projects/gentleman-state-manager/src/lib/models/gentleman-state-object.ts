2f1aa492bed26d001bf51926e68aa3bc37b22509
Alan Buscaglia <alanbuscaglia@singularcover.com>
2021-03-25 12:24:44 +0100

    fix: better implementation of checkIfPropertyExists

--- projects/gentleman-state-manager/src/lib/models/gentleman-state-object.ts
+++ projects/gentleman-state-manager/src/lib/models/gentleman-state-object.ts
@@ -112,7 +112,7 @@ export class GentlemanStateObject<T extends TypeWithKey<any>> {
   private checkIfPropertyExists(state: T, property: string): any {
     const condition = () => {
       const propertyValue = state[property];
-      return propertyValue || propertyValue === null;
+      return propertyValue || propertyValue !== undefined;
     };
     return checkIfConditionMet(() => condition(), 'Selected property not found ! check if the key is correct and exists');
   }
